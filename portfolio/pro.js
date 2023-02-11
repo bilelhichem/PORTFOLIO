4c5b36caee434d2af73a3901dbb9523d4cf20318
HICHEM BILEL <101928436+bilelhichem@users.noreply.github.com>
2023-02-11 04:10:06 +0100

    Add files via upload

---
+++ portfolio/pro.js
@@ -0,0 +1,85 @@
+const fb = document.querySelector("#fb");
+const insta = document.querySelector("#insta");
+const git = document.querySelector("#git");
+const linkdln = document.querySelector("#linkdln");
+
+
+const fb1 = document.querySelector('#fb1');
+const insta1 = document.querySelector("#insta1");
+const git1 = document.querySelector("#git1");
+const linkdln1 = document.querySelector("#linkdln1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fb.addEventListener("click", function() {
+    window.open("https://www.facebook.com/profile.php?id=100016438029582");
+
+  });
+
+
+  
+insta.addEventListener("click", function() {
+    window.open("https://www.instagram.com/bilel_hichem/?hl=fr");
+
+  });
+
+
+  
+git.addEventListener("click", function() {
+    window.open("https://github.com/bilelhichem");
+
+  });
+
+
+
+  
+linkdln.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/lakhmi-hichem-billal-a3b273255/");
+
+  });
+
+
+
+  fb1.addEventListener("click", function() {
+    window.open("https://www.facebook.com/profile.php?id=100016438029582");
+
+  });
+
+
+  
+insta1.addEventListener("click", function() {
+    window.open("https://www.instagram.com/bilel_hichem/?hl=fr");
+
+  });
+
+
+  
+git1.addEventListener("click", function() {
+    window.open("https://github.com/bilelhichem");
+
+  });
+
+
+
+  
+linkdln1.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/lakhmi-hichem-billal-a3b273255/");
+
+  });
+
+
+
